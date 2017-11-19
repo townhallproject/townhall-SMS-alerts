05af15c1a73a0f05b0fff5b1c5142df3ec277336
Darryl Davis <ddavis1306@gmail.com>
2017-11-18 21:23:37 -0800

    server exits after test finish using force exit

--- models/tests/sms.test.js
+++ models/tests/sms.test.js
@@ -5,22 +5,20 @@ const server = require('../../lib/server');
 
 describe('SMS', () => {
   beforeAll(() => {
-    server.start();
+    return server.start();
   });
+  afterAll(server.stop);
+
   describe('POST /sms', () => {
-    it('should respond with a 200 when there is an incoming zipcode', () => {
+    test('should respond with a 200 when there is an incoming zipcode', () => {
       let url = `http://localhost:3000/sms`;
       let incoming = 'Body=98122';
       return request.post(url)
-
         .send(incoming)
         .then(res => {
+          expect(res.text).toBe('<?xml version="1.0" encoding="UTF-8"?><Response><Message>There are not any upcoming town halls in your area.</Message></Response>');
           expect(res.status).toEqual(200);
         });
-
     });
-  });
-  afterAll(()=>{
-    server.stop();
   });
 });
