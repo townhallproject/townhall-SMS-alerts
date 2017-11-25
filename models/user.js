22cd8c5537feda5f7c961ecdbe2deaca163d957d
Darryl Davis <ddavis1306@gmail.com>
2017-11-25 11:10:44 -0800

    merged my code with Thaos

--- models/user.js
+++ models/user.js
@@ -1,8 +1,20 @@
 'use strict';
 
+const firebasedb = require('../lib/firebaseinit');
 module.exports = class User {
   constructor (phoneNumber, zipcode){
     this.phoneNumber = phoneNumber;
     this.zipcode = zipcode;
+    this.name = 'john';
+  }
+
+
+  writeToFirebase() {
+
+    let newPostKey = firebasedb.ref('sms-users').push().key;
+
+    let updates = {};
+    updates['/sms-users/' + newPostKey] = this;
+    return firebasedb.ref().update(updates);
   }
 };
