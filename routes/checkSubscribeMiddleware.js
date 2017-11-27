12bcaf3145fb4e2fe369f758fb8ef32bbc002d20
meganr <meganrm@gmail.com>
2017-11-26 19:12:33 -0800

    removed req.subscribed false

--- routes/checkSubscribeMiddleware.js
+++ routes/checkSubscribeMiddleware.js
@@ -1,12 +1,10 @@
 'use strict';
 
 module.exports = function(req, res, next){
-  req.subscribe = false;
   let response = req.body.Body.split(' ');
   if(response[0].toLowerCase() === 'subscribe'){
     req.subscribe = true;
   }
-  console.log('Subscribe: ' ,req.subscribe);
   console.log('reached subscribe check');
   return next();
 };
--- routes/sms.js
+++ routes/sms.js
@@ -12,12 +12,6 @@ const getEvents = require('./getEventsMiddleware');
 const checkSubscribe = require('./checkSubscribeMiddleware');
 const makeUser = require('./makeUserMiddleware');
 
-// const twilioAccountSid = process.env.TWILIO_ACCOUNT_ID;
-// const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
-// const client = require('twilio')(twilioAccountSid, twilioAuthToken);
-// const myTwilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
-// const jsonParser = require('body-parser').json();
-
 smsRouter.post('/sms',
   bodyParser,
   checkSubscribe,
@@ -25,36 +19,17 @@ smsRouter.post('/sms',
   townHallHandler.getDistricts,
   getEvents,
   (req, res) => {
-    if(req.subscribe === true){
+    if(req.subscribe){
       console.log('reached subscribe true call');
-      makeUser(req, res);
+      return makeUser(req, res);
     }
-    if(req.subscribe === false){
-      console.log('reached subscribe false call');
-      if (req.townHalls.length > 0) {
-        req.townHalls.forEach((townhall) => {
-          req.twiml.message(townhall.print());
-        });
-        req.twiml.message('That\'s all the events we have for your reps. Send subscribe <zip code> to get reoccuring updates.');
-        return messaging.end(res, req.twiml);
-      }
-      messaging.sendAndWrite(req, res, 'There are not any upcoming town halls in your area. Send subscribe <zip code> to get reoccuring updates.');
+    console.log('reached subscribe false call');
+    if (req.townHalls.length > 0) {
+      req.townHalls.forEach((townhall) => {
+        req.twiml.message(townhall.print());
+      });
+      req.twiml.message('That\'s all the events we have for your reps. Send subscribe <zip code> to get reoccuring updates.');
+      return messaging.end(res, req.twiml);
     }
+    messaging.sendAndWrite(req, res, 'There are not any upcoming town halls in your area. Send subscribe <zip code> to get reoccuring updates.');
   });
-
-// smsRouter.post('/broadcast',
-//   jsonParser,
-//   (req, res) => {
-//
-//     req.body.users.forEach((user) => {
-//
-//       client.messages.create({
-//         body: req.body.message,
-//         to: user.phoneNumber,
-//         from: myTwilioPhoneNumber,
-//       })
-//         .then((message) => process.stdout.write(message.sid));
-//     });
-//     res.send('Successfully Sent');
-//
-//   });
