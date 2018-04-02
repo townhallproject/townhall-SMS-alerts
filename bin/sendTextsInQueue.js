8fd2f8fc9eb27bca6375a0e183735b66c72cdf58
Megan Riel-Mehan <meganriel-mehan@Megans-MacBook-Pro.local>
2018-04-01 17:23:35 -0700

    checks if event is still in the future

--- bin/sendTextsInQueue.js
+++ bin/sendTextsInQueue.js
@@ -5,10 +5,15 @@ const messaging = require('../lib/response');
 firebasedb.ref('sms-queue').once('value').then((snapshot) => {
   snapshot.forEach((message) => {
     let messageData = message.val();
-    messaging.newMessage(messageData.body, messageData.phoneNumber)
-      .then((message) => {
-        process.stdout.write(message.sid);
-        //TODO: delete from queue
-      });
+    if (moment(messageData.dateObj).isAfter()){
+      messaging.newMessage(messageData.body, messageData.phoneNumber)
+        .then((message) => {
+          process.stdout.write(message.sid);
+          //TODO: delete from queue
+          firebasedb.ref(`sms-queue/${messageData.key}`).remove()
+        });
+    } else {
+      firebasedb.ref(`sms-queue/${messageData.key}`).remove()
+    }
   });
 });
