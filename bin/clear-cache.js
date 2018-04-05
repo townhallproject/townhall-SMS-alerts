560f079e8b04f735014a178b00b9c9c34ccee7c1
meganrm <meganrm@gmail.com>
2018-04-04 20:04:49 -0700

    cache expires after 10 mins

--- bin/clear-cache.js
+++ bin/clear-cache.js
@@ -6,7 +6,7 @@ const firebasedb = require('../lib/firebaseinit');
 firebasedb.ref('sms-users/cached-users').once('value').then((snapshot) => {
   snapshot.forEach((message) => {
     let user = message.val();
-    if (moment().diff(moment(user.last_updated), 'm') > 1){
+    if (moment().diff(moment(user.last_updated), 'm') > 10){
       let ref = firebasedb.ref(`sms-users/cached-users/${message.key}`);
       ref.remove();
     }
