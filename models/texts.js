a32b4a4a8ced487ed1fef9f3a7c503b6bd3b4598
meganrm <meganrm@gmail.com>
2018-07-19 11:22:44 -0700

    really doing it

--- models/texts.js
+++ models/texts.js
@@ -60,8 +60,7 @@ module.exports = class Text {
   }
   
   sendAlert(testingNumber){
-    // let sendingNumber = testing ? process.env.TESTING_NUMBER : this.phoneNumber;
-    let sendingNumber = process.env.TESTING_NUMBER;
+    let sendingNumber = testing ? process.env.TESTING_NUMBER : this.phoneNumber;
     let cacheNumber = testingNumber || sendingNumber;
     const thisAlert = this;
     return User.getUserFromCache(cacheNumber)
