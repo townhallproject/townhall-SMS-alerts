4bd8b80a6807adc06455ec62c04091528952a620
meganrm <meganrm@gmail.com>
2018-09-04 16:34:08 -0700

    remove deleted townhalls

--- database/firebaseListener.js
+++ database/firebaseListener.js
@@ -11,4 +11,22 @@ module.exports = function() {
         });
     }
   });
+  firebasedb.ref('townHalls').on('child_removed', (snapshot) => {
+    let townhall = new TownHall(snapshot.val());
+    townhall.removeFromQueue()
+      .catch((e) => {
+        console.log(e);
+      });
+  });
+  firebasedb.ref('sms-queue/').once('value').then(snapshot => {
+    snapshot.forEach(ele => {
+      let text = ele.val();
+      firebasedb.ref('townHalls/' + text.eventId).once('value').then(snapshot => {
+        if (!snapshot.exist()) {
+          console.log(ele.key);
+        }
+
+      });
+    });
+  });
 };
