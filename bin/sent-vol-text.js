8d42cdbad74b8a2ca61262e2c8bac31d90eafef1
meganrm <meganrm@gmail.com>
2019-07-19 14:55:57 -0400

    changing to production setup

--- bin/sent-vol-text.js
+++ bin/sent-vol-text.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
+const server = require('../lib/server.js');
+const express = require('express');
+
+const PORT =  5000;
+const app = express();
 
 const messaging = require('../lib/response');
 const User = require('../models/user');
@@ -9,37 +14,50 @@ const {
   VOL_RECRUIT,
 } = constants;
 
-const body = `Hi! This is Nathan with Town Hall Project. Our mission to hold our elected leaders accountable to the people they work for (the people!) is only possible with our volunteers.
-We're less than a month away from a 5-week Congressional recess -- when most of the town halls for the entire year are held, and we need YOUR help!
-Can you volunteer just 1 hour / week to help us make sure Americans are informed of crucial opportunities to Show Up and Speak Out.
-If you're interested, please contact us via this short form: bit.ly/2J3kQMD.
-With your help, we can take back our democracy and make this the country we know it can be.`;
-/* eslint-enable */
+const body = `Hi! Nathan here with Town Hall Project. A 5-week Congressional recess is almost here--when hundreds of town halls are held, and we need YOUR help!
+Can you volunteer just 1 hour / week from your own home to help us make sure Americans are informed of crucial opportunities to Show Up and Speak Out?
+There’s no easier way to play a powerful part in holding our elected officials accountable. To learn more, please reply back with YES.`;
 
 const sendMessageAndSave = (user) => {
-  messaging.newMessage(body, 'testing number')
-    .then(() => {
-      user.updateCache({
-        sessionType: VOL_RECRUIT,
-        messages: [{
-          body,
-          from_user: false,
-        }],
-      });
+  User.getUserFromCache(user.phoneNumber)
+    .then(cachedUser => {
+      // already sent alert to this user 
+      if (cachedUser.sessionType === VOL_RECRUIT) {
+        return;
+      }
+      messaging.newMessage(body, user.phoneNumber)
+        .then(() => {
+          user.updateCacheWithMessageInConvo(body, false, VOL_RECRUIT);
+        })
+        .catch((e) => console.log('ERROR SENDING MESSAGES', e));
     });
 };
 
-firebasedb.ref('/sms-users/all-users')
-  .then((snapshot) => {
-    snapshot.forEach((userSnap, index) => {
-      if (index < 10 ) {
-        let user = new User({
-          phoneNumber: userSnap.key,
-        });
-        setTimeout(() => {
-          sendMessageAndSave(user);
-        }, 1000 * index);
-      }
+const getUsers = () => {
+  firebasedb.ref('/sms-users/all-users').once('value')
+    .then((snapshot) => {
+      let index = 0;
+      snapshot.forEach((userSnap) => {
+        if (index < 250 ) {
+          let user = new User({
+            phoneNumber: userSnap.key,
+          });
+          setTimeout(() => {
+            sendMessageAndSave(user);
+          }, 1000 * index);
+        }
+        index++;
+      });
+    })
+    .catch(console.log);
+};
 
-    });
+server.start(app, PORT)
+  .then(console.log)
+  .then(() => {
+    console.log('getting users');
+    return getUsers();
+  })
+  .catch((e) => {
+    console.log(e);
   });
