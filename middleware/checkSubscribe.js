5dec1be991a38ccea3ef6e4b1d6603b66bf57285
meganrm <meganrm@gmail.com>
2019-07-19 14:58:22 -0400

    vol recruit setup

--- middleware/checkSubscribe.js
+++ middleware/checkSubscribe.js
@@ -1,7 +1,6 @@
 'use strict';
 const messaging = require('../lib/response');
 const scripts = require('../lib/scripts');
-
 const User = require('../models/user');
 
 const subscribeActions = require('../middleware/subscribeActions');
@@ -9,9 +8,10 @@ const deleteUser = require('../middleware/deleteUser');
 const userIsAttending = require('../middleware/userIsAttending');
 const unSubscribeFromRep = require('../middleware/unSubscribeFromRep');
 const constants = require('../constants');
-
+const production = process.env.NODE_ENV === 'production';
 const {
   ALERT_SENT,
+  VOL_RECRUIT,
 } = constants;
 
 module.exports = function(req, res, next){
@@ -26,12 +26,10 @@ module.exports = function(req, res, next){
   if (req.sessionType === ALERT_SENT) {
     if (response[0].toLowerCase() === 'y') {
       userIsAttending(req, res);
-    }
-    if (response[0].toLowerCase() === 'n') {
+    } else if (response[0].toLowerCase() === 'n') {
       req.twiml.message(scripts.notAttending);
       messaging.end(res, req.twiml);
-    }  
-    if (response.substring(0,3).toLowerCase() === 'not') {
+    } else if (response.substring(0,3).toLowerCase() === 'not') {
       unSubscribeFromRep(req, res);
     }
     return new User(req).deleteFromCache();
@@ -48,17 +46,23 @@ module.exports = function(req, res, next){
     new User(req).deleteFromCache();
     return;
   }
-
-  // start of vol recruit conversation 
-  if (req.sessionType === 'volRecruit') {
+  // vol recruit conversation 
+  if (req.sessionType === VOL_RECRUIT) {
     let user = new User(req);
-    let message = {
-      body: response,
-      from_user: true,
-    };
-    user.updateCache({
-      messages: [...req.messages, message],
-    });
+    // save message from user
+    user.updateCacheWithMessageInConvo(response, true);
+    // if simple response, send automatic response
+    if (req.messageCount === 1 && response.toLowerCase() === 'yes') {
+      // still save that we responded in the message flow
+      user.updateCacheWithMessageInConvo(scripts.willVolunteer, false, VOL_RECRUIT);
+      messaging.sendAndWrite(req, res, scripts.willVolunteer);
+      return;
+    }
+    if (production) {
+      // forward message to nathan's phone
+      messaging.newMessage(response, process.env.NATHAN);
+    }
+    messaging.end(res, req.twiml);
     return;
   }
   return next();
